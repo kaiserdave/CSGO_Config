fe366913e015823c84823021f92653fce677fa02
kaiserdave <kaiserdave@kaiserdave.com>
2018-01-12 14:30:23 -0800

    deploy to Steam in multiple possible paths to all local profiles

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,13 @@
-$steamprofiles = (gci "G:\Program Files\Steam\userdata\[0-9]*\730\local\cfg")
+$steampaths = ("C:\Program Files\Steam",
+    "C:\Program Files (x86)\Steam",
+    "G:\Program Files\Steam")
 
-foreach ($dir in $steamprofiles) {
-    copy *.cfg $dir
+foreach ($steampath in $steampaths) {
+    if ((Test-Path $steampath)) {
+        $csgoprofiles = (gci "${steampath}\userdata\[0-9]*\730\local\cfg")
+        foreach ($csgoprofile in $csgoprofiles) {
+            echo "Deploying to '${csgoprofile}'"
+            copy *.cfg $csgoprofile
+        }
+    }
 }
