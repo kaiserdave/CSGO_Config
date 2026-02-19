a6accce2e8d5080c7b516a56ce0386c47b43463c
kaiserdave <kaiserdave@kaiserdave.com>
2026-02-19 15:00:37 -0800

    Update the Steam path to the Games subdirectory

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export steamlib="/media/SteamLibrary"
+export steamlib="/media/Games/SteamLibrary"
 export cscfgdir="${steamlib}/steamapps/common/Counter-Strike Global Offensive/game/csgo/cfg"
 cp -p ./cfg/*.cfg "${cscfgdir}"
