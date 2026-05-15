c7476051f3520d3f33de2f7e2067bc4919442b70
kaiserdave <kaiserdave@kaiserdave.com>
2026-05-15 04:29:58 -0700

    Add a diff script

---
+++ diff.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+basedir="cfg"
+steamlib="/media/Games/SteamLibrary"
+cscfgdir="${steamlib}/steamapps/common/Counter-Strike Global Offensive/game/csgo/cfg"
+
+for F in `find ${basedir} -type f -not -name '*.orig' | sort`
+do
+	F=$(echo $F | sed "s;${basedir}/;;")
+	echo "Checking $F"
+	targetdir="${cscfgdir}/$F" 
+	test -f "${targetdir}" || echo "    Not found $F"
+	git diff "${targetdir}" "${basedir}/$F"
+done
+
